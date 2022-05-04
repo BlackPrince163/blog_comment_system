4d0c032f6d29cfce3eb3b2d5810eeb66eb563e32
BlackPrince163 <4523120100Ni>
2022-05-04 22:22:18 +0300

    + models

--- src/system_app/models.py
+++ src/system_app/models.py
@@ -1,3 +1,22 @@
+
+
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    content = models.TextField(max_length=1000)
+    datetime_publish = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.content}"
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
+    response_comment = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE, default=None)
+    content = models.TextField(max_length=200)
+    level_nest = models.PositiveSmallIntegerField(default=1, blank=True)
+    datetime_publish = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.response_comment}, f{self.content}" if self.response_comment else f"{self.content}"
