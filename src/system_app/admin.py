51518e4c6a0ac64e36813bedaeee64955605c4d8
BlackPrince163 <4523120100Ni>
2022-05-05 20:58:59 +0300

    + admin stackedinline

--- src/system_app/admin.py
+++ src/system_app/admin.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import User, Group
 
 from .models import Article, Comment
 
-admin.site.register(Article)
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = (CommentInline, )
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
 admin.site.unregister(User)
 admin.site.unregister(Group)
